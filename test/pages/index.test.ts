cc261fb198f39f355081459ecc6f598ee412a839
Aurelien B <61365539+Neltab@users.noreply.github.com>
2022-08-01 19:20:32 +0200

    Add some tests

--- test/pages/index.test.ts
+++ test/pages/index.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'bun:test'
-import app from "../src/server"
+import app from "../../src/server"
 
-describe('Server', () => {
-  it('Should return 200 Response', async () => {
+describe('Locale FR', () => {
+  it('Should say Bonjour', async () => {
     const req = new Request('http://localhost:3000/')
     const res = await app.fetch(req)
     expect(res.status).toBe(200)
